bf23fc498e48a3b0850220906304ac68ea64039a
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-11 22:03:45 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter number");
+scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("Even");
+  }
+  else
+  {
+    printf("Odd");
+  }
+getch();
+}
+  
+  
+  
